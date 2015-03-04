b35712408d687a77c1179feb2421c103d70d321b
Bill Alpert <william.alpert@barrons.com>
2015-03-04 12:19:26 -0500

    An additional install script
    For use at NICAR 2015 meeting of investigative reporters.

---
+++ rule605/data/gather_source/install_packages_two.R
@@ -0,0 +1,13 @@
+#
+#' ---
+#' title: "install_packages file"
+#' author: "Bill Alpert, Barron's, 1.212.416.2742, william.alpert@barrons.com"
+#' date: "last changed February 27, 2015"
+#' ---
+
+
+# DISCLAIMER: Barron's is sharing these files as pieces of journalism, in an attempt to make our reporting more transparent and our research reproducible.  We wrote them with care, but Dow Jones provides them as is and makes no guarantees.
+
+install.packages(c("reshape2", "plyr", "dplyr", "tidyr", "ggplot2", "magrittr", "xtable", "digest", "car"))
+
+# end
